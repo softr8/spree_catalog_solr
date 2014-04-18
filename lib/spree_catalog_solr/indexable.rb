47582c1f9405b6b8f73d6a2f006c0ce15f129323
Edwin Cruz <softr8@gmail.com>
2014-04-18 10:17:25 -0700

    Adding product properties, images and variants to the index

--- lib/spree_catalog_solr/indexable.rb
+++ lib/spree_catalog_solr/indexable.rb
@@ -10,6 +10,9 @@ module SpreeCatalogSolr
       def make_me_indexable
         searchable(auto_index: false, auto_remove: false) do
           integer :id, stored: true
+
+          double :price, stored: true
+
           text :name, stored: true
           text :description, stored: true
           string :slug, stored: true
@@ -23,6 +26,48 @@ module SpreeCatalogSolr
           string :taxons, stored: true, multiple: true do
             taxons.map(&:name)
           end
+
+          string :variants, stored: true do
+            ([master] + variants_and_option_values).inject({}) do |final, variant|
+              final[variant.id] = {
+                  option_text: variant.options_text,
+                  is_master: true,
+                  price: variant.price,
+                  sku: variant.sku,
+                  position: variant.position
+              }
+              final
+            end.to_json
+          end
+
+          string :images, stored: true do
+            (images + variant_images).inject([]) do |final, image|
+              final << [
+                  viewable_id: image.viewable_id,
+                  viewable_type: image.viewable_type,
+                  alt: image.alt,
+                  position: image.position,
+                  versions: {
+                      mini: image.attachment.url(:mini),
+                      small: image.attachment.url(:small),
+                      product: image.attachment.url(:product),
+                      large: image.attachment.url(:large),
+                      original: image.attachment.url(:original)
+                  }
+              ]
+              final
+            end.flatten.to_json
+          end
+
+          string :product_properties, stored: true do
+            product_properties.collect do |property|
+              {
+                  name: property.property.name,
+                  value: property.value
+              }
+            end.to_json
+          end
+
         end
       end
     end
